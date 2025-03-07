4aba1160c4d14f909faa29f21f571b135b128f18
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-07 19:22:06 +0530

    Updated features of JWT TOKEN

--- src/main/java/com/task_management_system/SERVICES/JWT/UserServiceImpl.java
+++ src/main/java/com/task_management_system/SERVICES/JWT/UserServiceImpl.java
@@ -15,20 +15,16 @@ public class UserServiceImpl implements  UserService{
 
     @Autowired
     private UserRepository userRepository;
-
-
     @Override
     public UserDetailsService userDetailsService() {
         return this::loadUserByUsername ;
     }
-
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         // Fetch user by email from the repository
         User user = userRepository.findFirstByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
         System.out.println("User found: " + email);
-
         // Return a Spring Security UserDetails object
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
@@ -36,7 +32,6 @@ public class UserServiceImpl implements  UserService{
                 List.of(new SimpleGrantedAuthority(user.getUserRole().name())) // Convert role to GrantedAuthority
         );
     }
-
    /* public UserDetailsService userDetailsService() {
         return this;
     }*/
