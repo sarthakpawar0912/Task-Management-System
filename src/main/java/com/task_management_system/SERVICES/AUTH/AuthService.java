b20db743a0e8f35fd357399a5f8c2398feabb70f
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-07 19:25:10 +0530

    Updated features of Authentication of Signup and Login

--- src/main/java/com/task_management_system/SERVICES/AUTH/AuthService.java
+++ src/main/java/com/task_management_system/SERVICES/AUTH/AuthService.java
@@ -6,9 +6,6 @@ import com.task_management_system.DTO.UserDto;
 
 public interface AuthService {
     void createAnAdminAccount();
-
     UserDto signupUser(SignupRequest signupRequest);
-
-
     boolean   hasUserWithEmail(String email);
 }
