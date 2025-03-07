6f81b76c4f3588e1584b854e465923eb9143356f
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-07 19:21:44 +0530

    Updated features of JWT TOKEN

--- src/main/java/com/task_management_system/UTILS/JwtUtil.java
+++ src/main/java/com/task_management_system/UTILS/JwtUtil.java
@@ -20,67 +20,52 @@ import java.util.function.Function;
 
 @Configuration
 public class JwtUtil {
-
     private final UserRepository userRepository;
-
     public JwtUtil(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
     public String generateToken(UserDetails userDetails){
         return generateToken(new HashMap<>(),userDetails);
     }
-
     private String generateToken(Map<String,Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
     }
-
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode("afafasfafafasfasfasfafacasdasfasxASFACASDFACASDFASFASFDAFASFASDAADSCSDFADCVSGCFVADXCcadwavfsfarvf");
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
     public boolean isTokenValid(String token, UserDetails userDetails){
         String username = extractUserName(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
-
     private String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
     }
-
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
-
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
-
     private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers) {
         final Claims claims = extractAllClaims(token);
         return claimsResolvers.apply(claims);
     }
-
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
     }
-
-
     public String extractUsername(String jwt) {
         {
             return extractClaim(jwt, Claims::getSubject);
         }
     }
-
     public boolean validateToken(String jwt, UserDetails userDetails) {
         String username = extractUserName(jwt);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(jwt));
     }
-
     public User getLoggedInUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String)) {
@@ -94,12 +79,4 @@ public class JwtUtil {
         }
         return null;
     }
-
-
-
-
-
-
-
-
 }
