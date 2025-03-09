b32d16b51b523619c29360dea4a8599cfd311832
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-09 19:43:32 +0530

    Made Change in JWT TOKEN APIs

--- src/main/java/com/task_management_system/CONFIGS/JwtAuthenticationFilter.java
+++ src/main/java/com/task_management_system/CONFIGS/JwtAuthenticationFilter.java
@@ -23,12 +23,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtil jwtUtil;
+
     @Autowired
     private UserService userService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-
         // Retrieve Authorization header
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
