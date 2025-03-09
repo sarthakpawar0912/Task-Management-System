0714d05681d7f0d8657f094169ee059a93eabe49
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-09 19:40:28 +0530

    Made Change in JWT UTILs

--- src/main/java/com/task_management_system/UTILS/JwtUtil.java
+++ src/main/java/com/task_management_system/UTILS/JwtUtil.java
@@ -20,6 +20,7 @@ import java.util.function.Function;
 
 @Configuration
 public class JwtUtil {
+
     private final UserRepository userRepository;
     public JwtUtil(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -33,50 +34,60 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
     }
+
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode("afafasfafafasfasfasfafacasdasfasxASFACASDFACASDFASFASFDAFASFASDAADSCSDFADCVSGCFVADXCcadwavfsfarvf");
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
     public boolean isTokenValid(String token, UserDetails userDetails){
         String username = extractUserName(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
+
     private String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
     }
+
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
+
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
+
     private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers) {
         final Claims claims = extractAllClaims(token);
         return claimsResolvers.apply(claims);
     }
+
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
     }
+
     public String extractUsername(String jwt) {
         {
             return extractClaim(jwt, Claims::getSubject);
         }
     }
+
     public boolean validateToken(String jwt, UserDetails userDetails) {
         String username = extractUserName(jwt);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(jwt));
     }
+
     public User getLoggedInUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String)) {
             // Cast principal to UserDetails
             org.springframework.security.core.userdetails.User userDetails =
                     (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
-
             // Fetch your custom User entity using the email (username)
             return userRepository.findFirstByEmail(userDetails.getUsername())
                     .orElse(null);
         }
         return null;
     }
+
 }
