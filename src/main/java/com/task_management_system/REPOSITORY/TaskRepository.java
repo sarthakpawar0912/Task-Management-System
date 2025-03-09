b53c7e55fc946a67d0027af58ca12f0aaff6fb24
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-09 19:28:35 +0530

    Made minute  changes in All Repositories

--- src/main/java/com/task_management_system/REPOSITORY/TaskRepository.java
+++ src/main/java/com/task_management_system/REPOSITORY/TaskRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Long> {
+
     List<Task> findAllByTitleContaining(String title);
 
     List<Task> findAllByUserId(Long id);
--- src/main/java/com/task_management_system/REPOSITORY/UserRepository.java
+++ src/main/java/com/task_management_system/REPOSITORY/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
     Optional<User> findFirstByEmail(String username);
+
     Optional<User> findByUserRole(UserRole userRole);
 }
