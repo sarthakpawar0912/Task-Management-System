3acdcfcbdd8b1266ddf62ca947ef8c3b1a4e8b77
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-14 20:14:19 +0530

    Fixed bugs and updated code

--- src/main/java/com/task_management_system/CONFIGS/JwtAuthenticationFilter.java
+++ src/main/java/com/task_management_system/CONFIGS/JwtAuthenticationFilter.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -30,6 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
+
         // Retrieve Authorization header
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
