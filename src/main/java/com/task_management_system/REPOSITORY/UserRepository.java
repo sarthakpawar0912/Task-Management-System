82beec4ac5f0afae6e964619155e10c070f9de75
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-14 20:15:28 +0530

    Fixed bugs and updated code

--- src/main/java/com/task_management_system/REPOSITORY/UserRepository.java
+++ src/main/java/com/task_management_system/REPOSITORY/UserRepository.java
@@ -13,4 +13,5 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findFirstByEmail(String username);
 
     Optional<User> findByUserRole(UserRole userRole);
+
 }
