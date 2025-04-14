0519f1e20fdd09c2205f754a3269eb54b4c5c48d
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-14 20:16:00 +0530

    Fixed bugs and updated code

--- src/main/java/com/task_management_system/UTILS/JwtHelper.java
+++ src/main/java/com/task_management_system/UTILS/JwtHelper.java
@@ -80,4 +80,5 @@ public class JwtHelper {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
+
 }
