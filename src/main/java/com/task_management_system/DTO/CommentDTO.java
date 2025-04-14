7140d73dddd4d72ce4753783c44e6588b4eeb73c
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-14 20:12:55 +0530

    Fixed bugs and updated code

--- src/main/java/com/task_management_system/DTO/CommentDTO.java
+++ src/main/java/com/task_management_system/DTO/CommentDTO.java
@@ -8,10 +8,15 @@ import java.util.Date;
 public class CommentDTO {
 
     private Long id;
+
     private String content;
+
     private Date createdAt;
+
     private Long taskId;
+
     private Long userId;
+
     private String postedBy;
 
 }
